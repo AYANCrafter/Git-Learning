6c9c8d05bfc994435f068dc2afa5fd78abdca91d
Ayan Sarkar <ayansarkar4005@gmail.com>
2025-03-21 10:58:48 +0530

    Payment updated after footer

--- first.js
+++ first.js
@@ -2,7 +2,14 @@ const button="Added a button";
 console.log(button);
 
 const login="Login page added";
+<<<<<<< HEAD
 console.log(login)
 
 const footer="Footer added in our website";
 console.log(footer)
+=======
+console.log(login);
+
+const Payment="Integrated the payment gateway";
+console.log(Payment)
+>>>>>>> f861baf (Payment Added)
