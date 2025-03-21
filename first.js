a689fb2d91eda775e082263b72336c8bd0b1f344
Ayan Sarkar <ayansarkar4005@gmail.com>
2025-03-21 11:53:54 +0530

    Latest changes

--- first.js
+++ first.js
@@ -2,21 +2,14 @@ const button="Added a button";
 console.log(button);
 
 const login="Login page added";
-<<<<<<< HEAD
 console.log(login)
 
 const footer="Footer added in our website";
 console.log(footer)
-=======
 console.log(login);
 
 const Payment="Integrated the payment gateway";
 console.log(Payment)
-<<<<<<< HEAD
->>>>>>> f861baf (Payment Added)
-=======
 
-const upi="Integrated the upi";
-console.log(upi);
 
->>>>>>> a85be69 (UPI Added)
+console.log("Latest Update");
